9ca053ec4031e9c6e142fbb7e7d9ac5d08c4bd83
shashanoid <shashanoid@gmail.com>
2024-12-10 01:39:55 -0500

    tets

---
+++ songs/index.js
@@ -0,0 +1,11 @@
+import one from "./one.mp3";
+import two from "./two.mp3";
+import three from "./three.mp3";
+import five from "./five.mp3";
+import six from "./six.mp3";
+import seven from "./seven.mp3";
+import eight from "./eight.mp3";
+import nine from "./nine.mp3";
+import ten from "./ten.mp3";
+
+export default { one, two, three, five, six, seven, eight, nine, ten };
